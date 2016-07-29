46d6626789315f4f55788393b2a407605651d00e
Andrew Bartuska <andrew.bartuska@duke.edu>
2016-07-29 15:30:19 -0400

    Fixed position of flicker

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def flicker(win, value):
 	circle = visual.Circle(win, units='height', radius=0.05,
     						fillColorSpace='rgb255',
     						lineColorSpace='rgb255',
-    						fillColor=(0, 0, 0), pos=(0.75, -0.45),
+    						fillColor=(0, 0, 0), pos=(0.73, 0.42),
     						lineColor=(0, 0, 0))
 	value = np.binary_repr(value)
 	# zero pad to 8 bits and add stop and start bits
